fa49247a9cac1be96cd8f84abbaed335c42eb53f
anouksha <anu_blue21@hotmail.com>
2014-06-10 12:31:16 -0700

    Testing out different tweet entities

--- tweetswithnumbers.py
+++ tweetswithnumbers.py
@@ -4,7 +4,6 @@ from tweepy import Stream
 from tweepy import OAuthHandler
 from tweepy.streaming import StreamListener
 import re
-import json
 import pymongo
 
 api_key = 'ARuQQNlhwQPF8X1zHbbQOGkJW'
@@ -12,28 +11,19 @@ api_secret = 'IboCMM6EjsBqaUlD2vLe4Crr1OtaDp58btKiYd7loUUTvDiUQM'
 access_token = '2536032672-a5X8UEDQhZxsaHHjYPcUmtQbuVfDlw27pPF89xp'
 access_secret = 'fQaBw3TG9K8eoMtV0MtBERJhCJJw1gBRY8aPaMtnqe1Qg'
 
-count=1
-
 class listener(StreamListener):
-
-
-    '''def on_data(self, data):
-
-        #print data
-        self.db = pymongo.MongoClient().test
-        self.db.tweets.insert(json.loads(data))
-        return True'''
 
     def on_status(self, status):
         phonePattern = re.compile(r'(\d{3})\D*(\d{3})\D*(\d{4})\D*(\d*)$')
         if phonePattern.search(status.text):
-            #self.db = pymongo.MongoClient().test
-            #self.db.phonetweets.insert(json.loads(status.text))
-            print str(status.from_user_name)+"\t"+str(status.created_at)+"\t"+status.text+"\t"+phonePattern.split(status.text)
-            '''data={}
+            print status.text
+            data={}
+            data['user'] = status.user
+            data['created_at'] = status.created_at
             data['text']=status.text
+            data['number'] = phonePattern.split(status.text)
             db = pymongo.MongoClient().test
-            db.phonetweets.insert(data)'''
+            db.phonetweets.insert(data)
 
     def on_error(self, status):
         print status
@@ -44,5 +34,3 @@ auth.set_access_token(access_token, access_secret)
 l=listener()
 twitterStream = Stream(auth, l)
 twitterStream.filter(track=["call", "text","dial"])
-
-
