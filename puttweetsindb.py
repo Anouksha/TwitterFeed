77bb36cf167f4cff42837d4d14f09109549b8587
anouksha <anu_blue21@hotmail.com>
2014-06-13 14:10:19 -0400

    Storing tweets from file into DB

--- puttweetsindb.py
+++ puttweetsindb.py
@@ -1,1 +1,18 @@
 __author__ = 'anouksha'
+
+import json
+import pymongo
+
+
+tweets = []
+
+for line in open('/home/anouksha/Documents/Special Problem/phone.20140414-034737.json'):
+  try:
+    tweets.append(json.loads(line))
+  except:
+    pass
+
+for tweet in tweets:
+    #print tweet
+    db = pymongo.MongoClient().sample
+    db.bharat_phonetweets.insert(tweet)
