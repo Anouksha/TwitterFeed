352248f0379e000d1c42df16ecc71bfe5b9f2876
anouksha <anu_blue21@hotmail.com>
2014-06-13 14:15:30 -0400

    Adding comments

--- puttweetsindb.py
+++ puttweetsindb.py
@@ -6,12 +6,16 @@ import pymongo
 
 tweets = []
 
+'''The for loop below will read each line of the file and attempt to parse the tweet data
+ using json.loads(), a function that converts raw JSON messages into objects. '''
+
 for line in open('/home/anouksha/Documents/Special Problem/phone.20140414-034737.json'):
   try:
     tweets.append(json.loads(line))
   except:
     pass
 
+#storing each tweet into the bharat_phonetweets collection in the sample DB
 for tweet in tweets:
     #print tweet
     db = pymongo.MongoClient().sample
