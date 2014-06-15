96a69fa281aabbe6fc206568d13e8df4be2175f6
anouksha <anu_blue21@hotmail.com>
2014-06-15 11:42:30 -0400

    Added phone regex

--- testtweets.py
+++ testtweets.py
@@ -1,9 +1,15 @@
 __author__ = 'anouksha'
 
 import pymongo
+import re
 
 db = pymongo.MongoClient().sample
 tweets = db.hundred_tweets.find()
 
+phonePattern = re.compile(r'(\d{3})\D*(\d{3})\D*(\d{4})\D*(\d*)$')
+count = 1
 for tweet in tweets:
-    print tweet['text']
+    #print tweet['text']
+    if phonePattern.search(tweet['text']):
+        print str(count)+". "+tweet['text']
+    count +=1
