af8fe935f0c184c455b22df2faa06f224996f3ee
Anouksha <anu_blue21@hotmail.com>
2014-06-26 04:04:39 +0000

    Modified script to dump tweets from file into DB

--- puttweetsindb.py
+++ puttweetsindb.py
@@ -2,21 +2,34 @@ __author__ = 'anouksha'
 
 import json
 import pymongo
+from os import listdir
 
+db = pymongo.MongoClient().tweets
+path = '/home/bharat/data/twitter'
+count = 0
 
-tweets = []
+#This for loop will iterate through each file in the directory
 
-'''The for loop below will read each line of the file and attempt to parse the tweet data
- using json.loads(), a function that converts raw JSON messages into objects. '''
+for f in listdir(path):
 
-for line in open('/home/anouksha/Documents/Special Problem/phone.20140414-034737.json'):
-  try:
-    tweets.append(json.loads(line))
-  except:
-    pass
+  '''The for loop below will read each line of the file and attempt to parse the tweet data
+  using json.loads(), a function that converts raw JSON messages into objects. '''
 
-#storing each tweet into the bharat_phonetweets collection in the sample DB
-for tweet in tweets:
-    #print tweet
-    db = pymongo.MongoClient().sample
-    db.bharat_phonetweets.insert(tweet)
+  count = count + 1
+  if count > 373:
+    tweets=[]
+    filepath = path+'/'+f
+    print str(count) + ". "+filepath
+    for line in open(filepath):
+      try:
+        tweets.append(json.loads(line))
+      except:
+        pass
+
+    #storing each tweet into the bharat_phonetweets collection in the tweets DB
+    for tweet in tweets:
+      #print tweet
+      #db = pymongo.MongoClient().tweets
+      db.bharat_phonetweets.insert(tweet)
+ 
+print "Done"
