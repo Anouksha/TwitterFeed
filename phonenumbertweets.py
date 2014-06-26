b3a1ae8df52640ab0c90270d5f06ebeb79551598
anouksha <anu_blue21@hotmail.com>
2014-06-26 17:10:53 -0400

    Changing time interval to one minute for gathering statistics

--- phonenumbertweets.py
+++ phonenumbertweets.py
@@ -7,6 +7,7 @@ import re
 import pymongo
 import json
 import time
+import datetime
 
 api_key = 'ARuQQNlhwQPF8X1zHbbQOGkJW'
 api_secret = 'IboCMM6EjsBqaUlD2vLe4Crr1OtaDp58btKiYd7loUUTvDiUQM'
@@ -15,35 +16,40 @@ access_secret = 'fQaBw3TG9K8eoMtV0MtBERJhCJJw1gBRY8aPaMtnqe1Qg'
 
 class listener(StreamListener):
 
-    '''def __init__(self):
-        self.count = 1
+    def __init__(self):
+        self.count = 0
+        self.sec = 1
         #self.api=api
-        self.filename = "phone-"+time.strftime('%d-%m-%Y:%H:%M:%S')
-        self.text=""
-        #self.output = open(self.filename, 'a')'''
+        self.filename = "statistics"
+        self.start_time = datetime.datetime.now()
+        #self.output = open(self.filename, 'a')
 
     def on_data(self, data):
 
         phonePattern = re.compile(r'(\d{3})\D*(\d{3})\D*(\d{4})\D*(\d*)$')
         m = phonePattern.search(json.loads(data)['text'])
         if m:
-            print json.loads(data)['text']
-            #print status
-            db = pymongo.MongoClient().tweets
-            db.phone_numbers.insert(json.loads(data))
-            #output = open(self.filename, 'a')
-            '''self.text = self.text+str(self.count)+". "+json.loads(data)['text']
-            print str(self.count)+". "+json.loads(data)['text']
-            self.count += 1'''
+            try:
+                self.count += 1
+                text = str(self.count)+". "+json.loads(data)['text']
+                print text
+                #print status
+                db = pymongo.MongoClient().tweets
+                db.phone_numbers.insert(json.loads(data))
+                t=datetime.datetime.now()
+                if (t-self.start_time) > datetime.timedelta(0,60):
+                    output = open(self.filename, 'a')
+                    output.write(time.strftime('%d-%m-%Y:%H:%M:%S')+"\t"+str(self.sec)+"\t"+str(self.count)+"\n")
+                    self.count = 0
+                    self.sec += 1
+                    self.start_time = datetime.datetime.now()
+                    output.close()
+            except:
+                pass
         return True
 
     def on_error(self, status):
         print status
-
-    '''def write_data(self, text):
-        output = open(self.filename, 'a')
-        output.write(text+"\n")
-        output.close()'''
 
 
 auth=OAuthHandler(api_key, api_secret)
@@ -58,6 +64,5 @@ try:
                             "yellowpages","yellowpage", "fraud", "customer service", "landline",
                             "toll-free", "toll free", "complaint", "complaints", "tele", "cell phone",
                             "1-800","1-866","1-888","fax", "voice", "land line", "mobile", "ext"])
-    #l.write_data(l.text)
 except:
     pass
