0d27a8c4cfa710f9e2524a0ff85c66d882c49eb8
anouksha <anu_blue21@hotmail.com>
2014-06-25 20:12:46 -0400

    Test code

---
+++ testsched.py
@@ -0,0 +1,21 @@
+__author__ = 'anouksha'
+
+'''from apscheduler.scheduler import Scheduler
+
+# Start the scheduler
+sched = Scheduler()
+sched.start()
+
+def job_function():
+    print "Hello World"
+
+
+sched.add_cron_job(job_function, second = '*/10')'''
+
+'''import time
+
+def job__function():
+    print "Hello World"
+
+job__function()
+time.sleep(10)'''
