846221e230401b88a69cd8266c4f9a12f3417a78
anouksha <anu_blue21@hotmail.com>
2014-07-01 19:49:55 -0400

    Updated phone regex

--- testtweets.py
+++ testtweets.py
@@ -6,11 +6,11 @@ import re
 db = pymongo.MongoClient().sample
 tweets = db.hundred_tweets.find()
 
-phonePattern = re.compile(r'(\d{3})\D*(\d{3})\D*(\d{4})')
+phonePattern = re.compile(r'[+]?([0-9]+)?[\(.*=$,~ \n\t-]?(\d{3})[\(\).*=$,~ \n\t-]?\s*(\d{3})[\(\).*=$,~ \n\t-]?\s*(\d{4})\d*')
 #phonePattern = re.compile('(([+]?([0-9]+)[.*=$, \n\t-])?\\s*\\(?\\s*([0-9])\\s*([0-9])\\s*([0-9])\\s*\\)?\\s*[.*=$, \n\t-]\\s*([0-9])\\s*([0-9])\\s*([0-9])\\s*[.*=$, \n\t-]\\s*([0-9])\\s*([0-9])\\s*([0-9])\\s*([0-9]))')
 count = 1
 for tweet in tweets:
     #print tweet['text']
     if phonePattern.search(tweet['text']):
         print str(count)+". "+tweet['text'] + "\tNumber: "+ phonePattern.search(tweet['text']).group()
-        count +=1
+    count +=1
