7c6ed63fbbefd318ccd4932e8863e1824d636ad8
anouksha <anu_blue21@hotmail.com>
2014-07-02 15:49:53 -0400

    Collecting statistics on phone numbers

--- phonestats.py
+++ phonestats.py
@@ -3,16 +3,21 @@ __author__ = 'anouksha'
 import pymongo
 import re
 
-db = pymongo.MongoClient().tweets
+db = pymongo.MongoClient().Twitter
 
 nums = db.numbers.distinct("number")
 nc = 0
-print "Distinct Numbers:"
+
 for num in nums:
-    nc += 1
-    accounts =  (db.numbers.find({"number":num})).distinct("name")
-    #text = str(nc) +". " +num+"\tCount:"+str(db.numbers.find({"number":num}).count())+"\tNum of Accounts: "+str(len(accounts))
-    data = {}
-    data['number'] = num
-    data['count'] = db.numbers.find({"number":num}).count()
-    db.phone_stats.insert()
+    if num != "no match":
+        nc += 1
+        accounts =  (db.numbers.find({"number":num})).distinct("name")
+        #text = str(nc) +". " +num+"\tCount:"+str(db.numbers.find({"number":num}).count())+"\tNum of Accounts: "+str(len(accounts))
+        data = {}
+        data['number'] = num
+        data['count'] = db.numbers.find({"number":num}).count()
+        data['accounts'] = accounts
+        data['accounts_num'] = len(accounts)
+        db.phone_stats.insert(data)
+
+print "Distinct Numbers: "+str(nc)
