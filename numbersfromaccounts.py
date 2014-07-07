3635191c16025bf2e0b5747ed8b4cefff0e669c7
anouksha <anu_blue21@hotmail.com>
2014-07-07 16:21:36 -0400

    Added files for gathering statistics

---
+++ numbersfromaccounts.py
@@ -0,0 +1,14 @@
+__author__ = 'anouksha'
+
+import pymongo
+import sys
+
+db = pymongo.MongoClient().Twitter
+users = db.numbers.distinct("screen_name")
+
+for u in users:
+    try:
+        numbers = db.numbers.find({"screen_name":u}).distinct("number")
+        print u+"\tNumbers: "+str(numbers)
+    except:
+        print sys.exc_info()
