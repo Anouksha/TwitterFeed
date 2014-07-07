4065051eacaef9036dce59c017c9b9ff2e291b36
anouksha <anu_blue21@hotmail.com>
2014-07-07 16:23:39 -0400

    Minor change in print statement

--- numbersfromaccounts.py
+++ numbersfromaccounts.py
@@ -9,6 +9,6 @@ users = db.numbers.distinct("screen_name")
 for u in users:
     try:
         numbers = db.numbers.find({"screen_name":u}).distinct("number")
-        print u+"\tNumbers: "+str(numbers)
+        print u+"\tNumbers: "+str(len(numbers))
     except:
         print sys.exc_info()
