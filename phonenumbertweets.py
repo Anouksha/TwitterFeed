5b30b0bfc1358311024dd1cd8bcce27f00ab1d41
anouksha <anu_blue21@hotmail.com>
2014-07-12 12:46:09 -0400

    Handling Exceptions

--- phonenumbertweets.py
+++ phonenumbertweets.py
@@ -46,58 +46,59 @@ class listener(StreamListener):
     def on_data(self, data):
 
         #phonePattern = re.compile(r'[+]?([0-9]+)?[\(.*=$,~ \n\t-]?\s*(\d{3})\s*\)?[\\\/.*=$,~ \n\t-]?\s*\(?(\d{3})\s*\)?[\\\/.*=$,~ \n\t-]?\s*(\d{4})\d*')
-        m = self.phonePattern.search(json.loads(data)['text'])
-        if m:
-            try:
-                self.count += 1
-                text = str(self.count)+". "+json.loads(data)['text']
-                print text
-                #print status
-                #db = pymongo.MongoClient().Twitter
-                self.db.tweets.insert(json.loads(data))
-                number = m.group().strip()
-                data1={}
-                data1['name'] = json.loads(data)['user']['name']
-                data1['screen_name'] = json.loads(data)['user']['screen_name']
-                data1['created_at'] = json.loads(data)['created_at']
-                data1['timezone'] = json.loads(data)['user']['time_zone']
-                data1['text']=json.loads(data)['text']
-                data1['verified'] = json.loads(data)['user']['verified']
-                data1['number'] = number
-                self.db.numbers.insert(data1)
+        if 'text' in data:
+            m = self.phonePattern.search(json.loads(data)['text'])
+            if m:
+                try:
+                    self.count += 1
+                    text = str(self.count)+". "+(json.loads(data)['text']).encode('utf-8')
+                    print text
+                    #print status
+                    #db = pymongo.MongoClient().Twitter
+                    self.db.tweets.insert(json.loads(data))
+                    number = m.group().strip()
+                    data1={}
+                    data1['name'] = json.loads(data)['user']['name']
+                    data1['screen_name'] = json.loads(data)['user']['screen_name']
+                    data1['created_at'] = json.loads(data)['created_at']
+                    data1['timezone'] = json.loads(data)['user']['time_zone']
+                    data1['text']=json.loads(data)['text']
+                    data1['verified'] = json.loads(data)['user']['verified']
+                    data1['number'] = number
+                    self.db.numbers.insert(data1)
 
-                c = self.db.phone_stats.find({"number": number}).count()
-                if c>0:
-                    num = self.db.phone_stats.find({"number": number})
-                    for n in num:
-                        count = n['count'] + 1
-                        accounts = (self.db.numbers.find({"number": number})).distinct("name")
-                        self.db.phone_stats.update({"number": number},{"$set": {"count": count, "accounts": accounts, "accounts_num":len(accounts)}})
+                    c = self.db.phone_stats.find({"number": number}).count()
+                    if c>0:
+                        num = self.db.phone_stats.find({"number": number})
+                        for n in num:
+                            count = n['count'] + 1
+                            accounts = (self.db.numbers.find({"number": number})).distinct("name")
+                            self.db.phone_stats.update({"number": number},{"$set": {"count": count, "accounts": accounts, "accounts_num":len(accounts)}})
 
-                    #db.phone_stats.update({"number": number},{"$set": {"accounts": accounts}})
-                    #db.phone_stats.update({"number": number},{"$set": {"accounts_num":len(accounts)}})
-                else:
-                    data2 = {}
-                    accounts = (self.db.numbers.find({"number":number})).distinct("name")
-                    data2['number'] = number
-                    data2['count'] = self.db.numbers.find({"number":number}).count()
-                    data2['accounts'] = accounts
-                    data2['accounts_num'] = len(accounts)
-                    self.db.phone_stats.insert(data2)
+                        #db.phone_stats.update({"number": number},{"$set": {"accounts": accounts}})
+                        #db.phone_stats.update({"number": number},{"$set": {"accounts_num":len(accounts)}})
+                    else:
+                        data2 = {}
+                        accounts = (self.db.numbers.find({"number":number})).distinct("name")
+                        data2['number'] = number
+                        data2['count'] = self.db.numbers.find({"number":number}).count()
+                        data2['accounts'] = accounts
+                        data2['accounts_num'] = len(accounts)
+                        self.db.phone_stats.insert(data2)
 
-                '''t=datetime.datetime.now()
-                if (t-self.start_time) > datetime.timedelta(0, 59, 0, 0, 1, 0, 0):
-                    output = open(self.filename, 'a')
-                    output.write(time.strftime('%d-%m-%Y:%H:%M:%S')+"\tCount: "+str(self.count)+"\n")
-                    self.count = 0
-                    self.hour += 1
-                    self.start_time = datetime.datetime.now()
-                    output.close()
-                    raise SystemExit'''
-            except Exception:
-                print sys.exc_info()
-                pass
-		#pass
+                    '''t=datetime.datetime.now()
+                    if (t-self.start_time) > datetime.timedelta(0, 59, 0, 0, 1, 0, 0):
+                        output = open(self.filename, 'a')
+                        output.write(time.strftime('%d-%m-%Y:%H:%M:%S')+"\tCount: "+str(self.count)+"\n")
+                        self.count = 0
+                        self.hour += 1
+                        self.start_time = datetime.datetime.now()
+                        output.close()
+                        raise SystemExit'''
+                except Exception:
+                    print sys.exc_info()
+                    pass
+            #pass
         return True
 
     def on_error(self, status):
