42dc6af703e37bc2cb37b1282293c57b1148b086
anouksha <anu_blue21@hotmail.com>
2014-07-16 00:02:32 -0400

    Change in regex to add space

--- whatsapp.py
+++ whatsapp.py
@@ -4,7 +4,7 @@ import pymongo
 
 db = pymongo.MongoClient().Twitter
 
-numbers_1 = db.numbers.find({"text":{"$regex":"[sS][Mm][Ss]/[Ww][Aa]"}}).sort([("count",pymongo.DESCENDING)]).distinct("number")
+numbers_1 = db.numbers.find({"text":{"$regex":"[sS][Mm][Ss]\s*/\s*[Ww][Aa]"}}).sort([("count",pymongo.DESCENDING)]).distinct("number")
 numbers_2 = db.numbers.find({"text":{"$regex":"[Ww]h[a]?ts[a]?pp"}}).sort([("count",pymongo.DESCENDING)]).distinct("number")
 numbers = numbers_1 + list(set(numbers_2) - set(numbers_1))
 print "Count:"+str(len(numbers))
