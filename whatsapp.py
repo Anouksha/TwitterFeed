f0c6f110bacd1779971eaaa86b394a88ad73d64f
anouksha <anu_blue21@hotmail.com>
2014-07-17 12:07:23 -0400

    Added count to be displayed

--- whatsapp.py
+++ whatsapp.py
@@ -9,4 +9,6 @@ numbers_2 = db.numbers.find({"text":{"$regex":"[Ww]h[a]?ts[a]?pp"}}).sort([("cou
 numbers = numbers_1 + list(set(numbers_2) - set(numbers_1))
 print "Count:"+str(len(numbers))
 for num in numbers:
-    print num
+    data = db.phone_stats.find({"number":num})
+    for d in data:
+        print num + "\t"+str(d['count'])
